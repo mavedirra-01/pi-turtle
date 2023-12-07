cb69d3c0692543c2897bf84bc2b87c05cf4c63f9
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2023-12-07 10:21:14 -0400

    Update test.sh

--- modules/test.sh
+++ modules/test.sh
@@ -2,6 +2,10 @@
 # Dependencies: feroxbuster, gedit
 # Inputs: url, extentions
 # Help: extentions - Options are 'txt, php, py, etc'
+# Silent: true
+# Logfile: /opt/pi-turtle/logs/test.log
+# Follow_log: true
+
 url=$1
 extentions=$2
 
