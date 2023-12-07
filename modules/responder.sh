3c846b8032b24d092bb418a448f1d7cf66dc203c
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2023-12-06 21:33:49 -0400

    Create responder.sh

---
+++ modules/responder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Dependencies: responder
+# Silent: true
+# Logfile: /opt/pi-turtle/logs/responder.log
+
+# Example command (replace with actual command to run responder)
+responder -I wlan0 -wd
