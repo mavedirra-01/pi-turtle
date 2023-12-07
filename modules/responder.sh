0e6e84fcce2fff6b1deae27a0ea8ea49ccc3da15
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2023-12-06 23:29:10 -0400

    Update responder.sh

--- modules/responder.sh
+++ modules/responder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Dependencies: responder
 # Silent: true
-# Logfile: /opt/pi-turtle/logs/responder.log
+# Logfile: /usr/share/responder/logs/Responder-Session.log
 
 echo "Responder script started"
-responder -I wlan0 -wd | tee -a /opt/pi-turtle/logs/responder.log
+responder -I wlan0 -wd
