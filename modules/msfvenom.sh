2a600b3b954dca9f9f716e6e49f123e9dd363a91
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2023-12-07 18:15:31 -0400

    Update msfvenom.sh

--- modules/msfvenom.sh
+++ modules/msfvenom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Dependencies: msfvenom
-# Inputs: PAYLOAD, LHOST, LPORT, OUTPUT_FILE
+# Inputs: PAYLOAD, LHOST, LPORT, FORMAT, OUTPUT_FILE
 # Help: Example Payload: python/meterpreter/reverse_tcp windows/meterpreter/reverse_tcp  Format options: exe, bat, raw, elf
 
 PAYLOAD="$1"
