2cb003c54eddd693cfe88feac0fb618c429ce81f
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2023-12-07 10:36:31 -0400

    Update test.sh

--- modules/test.sh
+++ modules/test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# Dependencies: feroxbuster gedit
-# Inputs: url, extentions
-# Help: extentions - Options are 'txt, php, py, etc'
+# Dependencies: nikto
+# Inputs: url
 # Silent: true
 # Logfile: /opt/pi-turtle/logs/test.log
 # Follow_log: true
 
 url=$1
-extentions=$2
-
-feroxbuster --url $url -x $extentions -w /opt/pi-turtle/wordlists/words.txt
+nikto -h $url
