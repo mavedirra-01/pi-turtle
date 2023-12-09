1352b8618f5af13bb3de4a3f60d31be4d17702ad
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2023-12-09 14:52:36 -0400

    Update simple-nmap.sh

--- modules/simple-nmap.sh
+++ modules/simple-nmap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Dependencies: nmap
 # Inputs: target, scan_type
-# Help: scan_type - Options are 'Quick Scan' or 'Intense Scan'
+# Help: scan_type - Options are 'QS' or 'IS'
+# Logfile: logs/nmap.log
+# Silent: true
 
 # Read arguments
 target=$1
@@ -9,7 +11,7 @@ scan_type=$2
 
 # Perform nmap scan based on the provided scan type
 case $scan_type in
-    "Quick Scan") nmap -T4 -F $target ;;
-    "Intense Scan") nmap -T4 -A $target ;;
+    "QS") nmap -T4 -F $target ;;
+    "IS") nmap -T4 -A $target ;;
     *) echo "Invalid scan type"; exit 1 ;;
 esac
